b14154c31323868b83d6b39d43b1874e686022d8
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-05-22 14:45:14 +0300

    trainers for main is done

--- src/reusables/TrainerInfo/TrainerInfo.tsx
+++ src/reusables/TrainerInfo/TrainerInfo.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import { TrainerInfoType } from '../../types/TrainerInfoType';
 
+import { FourthPart } from './components/FourthPart';
 import { Label } from './components/Label';
 import { SecondPart } from './components/SecondPart';
+import { ThirdPart } from './components/ThirdPart';
 
 import styles from './TrainerInfo.module.scss';
 
@@ -17,12 +19,16 @@ export const TrainerInfo: React.FC<Props> = React.memo(props => {
     : {};
 
   const secondPartProps = { name, trainingTypes, bio };
+  const thirdPartProps = { categories };
+  const fourthPartProps = { reviews };
 
   return (
     <div className={styles.trainer} style={trainerCssProps}>
       {isNew && <Label />}
       <div className={styles.avatar} />
       <SecondPart {...secondPartProps} />
+      <ThirdPart {...thirdPartProps} />
+      <FourthPart {...fourthPartProps} />
     </div>
   );
 });
