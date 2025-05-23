44258e8fad895b14c47995ad1818bd1c5020e834
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-05-23 09:45:11 +0300

    add wrapper for tp content

--- src/modules/MainContent/components/ProfileMain/ProfileMain.tsx
+++ src/modules/MainContent/components/ProfileMain/ProfileMain.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Events } from './components/Events';
 import { Trainers } from './components/Trainers';
 
+import styles from './ProfileMain.module.scss';
+
 export const ProfileMain: React.FC = () => {
   return (
-    <>
+    <div className={styles['profile-main']}>
       <Trainers />
       <Events />
-    </>
+    </div>
   );
 };
