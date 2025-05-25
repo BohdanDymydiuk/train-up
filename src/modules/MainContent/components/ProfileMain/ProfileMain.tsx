c471c46e7193a6f2ebb23aad59800afcce31a2be
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-05-25 22:19:42 +0300

    add location detect

--- src/modules/MainContent/components/ProfileMain/ProfileMain.tsx
+++ src/modules/MainContent/components/ProfileMain/ProfileMain.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Events } from './components/Events';
 import { Trainers } from './components/Trainers';
@@ -6,8 +6,40 @@ import { Trainers } from './components/Trainers';
 import styles from './ProfileMain.module.scss';
 
 export const ProfileMain: React.FC = () => {
+  const [location, setLocation] = useState('');
+
+  useEffect(() => {
+    function showCity(position: GeolocationPosition) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const city = data.address.city;
+          const village: string = data.address.village;
+
+          setLocation(city || village);
+        })
+        .catch(error => console.log(error));
+    }
+
+    navigator.geolocation.getCurrentPosition(showCity);
+  }, []);
+
   return (
     <div className={styles['profile-main']}>
+      <div
+        style={{
+          position: 'absolute',
+          color: 'white',
+          backgroundColor: 'black',
+        }}
+      >
+        You're located in {location}
+      </div>
       <Trainers />
       <Events />
     </div>
