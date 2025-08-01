cb2ac8555d870b7195e2158f555278f20691db07
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-08-01 12:47:35 +0300

    add correct origin to sc.java

--- backend/src/main/java/com/example/trainup/config/SecurityConfig.java
+++ backend/src/main/java/com/example/trainup/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
         List<String> allowedOrigins = new ArrayList<>(List.of(
                 "http://localhost:5173",
                 "https://c3e3-109-200-252-189.ngrok-free.app/",
-                "https://train-up-website.onrender.com"
+                "https://train-up.onrender.com/"
         ));
         //TODO: allowedOrigins.add("null"); only for testing
         allowedOrigins.add("null");
