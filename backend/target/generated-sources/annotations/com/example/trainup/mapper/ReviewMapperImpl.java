93cf64aad882eeb03a41195066e5f3f5865af629
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-08-03 20:57:31 +0300

    change db

--- backend/target/generated-sources/annotations/com/example/trainup/mapper/ReviewMapperImpl.java
+++ backend/target/generated-sources/annotations/com/example/trainup/mapper/ReviewMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-02T19:41:56+0000",
+    date = "2025-08-03T17:47:45+0000",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.15 (Ubuntu)"
 )
 @Component
