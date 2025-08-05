5044fda260473bb75713418ae529b4f278305dea
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-08-05 21:30:44 +0300

    remove ${PORT:-10000}

--- backend/target/generated-sources/annotations/com/example/trainup/mapper/EventMapperImpl.java
+++ backend/target/generated-sources/annotations/com/example/trainup/mapper/EventMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-05T16:40:29+0000",
+    date = "2025-08-05T18:25:53+0000",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.15 (Ubuntu)"
 )
 @Component
