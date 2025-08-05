fe46abf9bd2ef7fba6c305e9a5dbc52f5398ee20
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-08-05 19:30:30 +0300

    EXPOSE ${PORT:-10000}

--- backend/target/generated-sources/annotations/com/example/trainup/mapper/AthleteMapperImpl.java
+++ backend/target/generated-sources/annotations/com/example/trainup/mapper/AthleteMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-05T16:12:36+0000",
+    date = "2025-08-05T16:27:01+0000",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.15 (Ubuntu)"
 )
 @Component
