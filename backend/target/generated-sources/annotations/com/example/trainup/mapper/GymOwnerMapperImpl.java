1be9c4d7a10f29b1ee915616419be9ee77748770
Bohdan Shevtsov <shevtsov.b@gmail.com>
2025-08-09 16:50:20 +0200

    EventController GET method allowed for unregistered users

--- backend/target/generated-sources/annotations/com/example/trainup/mapper/GymOwnerMapperImpl.java
+++ backend/target/generated-sources/annotations/com/example/trainup/mapper/GymOwnerMapperImpl.java
@@ -14,8 +14,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-06T14:18:23+0000",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.15 (Ubuntu)"
+    date = "2025-08-09T16:34:34+0200",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
 )
 @Component
 public class GymOwnerMapperImpl implements GymOwnerMapper {
