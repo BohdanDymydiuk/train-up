91eca5ac6e8a1225dcb6061299c2a550dab45535
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-08-10 12:34:54 +0300

    add footer

--- frontend/src/modules/MainContent/components/Footer/Footer.tsx
+++ frontend/src/modules/MainContent/components/Footer/Footer.tsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { MainContext } from '../../../../context/MainContext';
+import { NavItems } from '../../../../enums/NavItems';
+import { ErmilovTitle } from '../../../../reusables/ErmilovTitle';
+import { FacebookSVG } from '../../../../reusables/svgs/socials/FacebookSVG';
+import { InstagramSVG } from '../../../../reusables/svgs/socials/InstagramSVG';
+import { ThreadsSVG } from '../../../../reusables/svgs/socials/ThreadsSVG';
 
 import styles from './Footer.module.scss';
 
 export const Footer: React.FC = () => {
-  return <footer className={styles.footer}></footer>;
+  const { onDesktop } = useContext(MainContext);
+
+  const pages = Object.values(NavItems).filter(
+    page => page !== NavItems.aboutUs,
+  );
+
+  const socials = {
+    instagram: <InstagramSVG />,
+    threads: <ThreadsSVG />,
+    facebook: <FacebookSVG />,
+  };
+
+  const keys = Object.keys(socials);
+  const values = Object.values(socials);
+
+  const ermilovTitleProps = {
+    title: 'Твоя форма. Твій темп. Твій простір.',
+    cssProps: { fontSize: onDesktop && '40px' } as React.CSSProperties,
+  };
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles['content-wrapper']}>
+        <div className={styles['title-wrapper']}>
+          <ErmilovTitle {...ermilovTitleProps} />
+        </div>
+
+        <div className={styles['pages-socials-wrapper']}>
+          <div className={styles['pages-wrapper']}>
+            <div className={styles['pages-text']}>Сторінки</div>
+
+            <nav className={styles.pages}>
+              <ul>
+                {pages.map(page => {
+                  return (
+                    <li className={styles.page} key={page}>
+                      {page}
+                    </li>
+                  );
+                })}
+              </ul>
+            </nav>
+          </div>
+
+          <div className={styles['socials-wrapper']}>
+            <div className={styles['socials-text']}>Ми в соцмережах</div>
+
+            <div className={styles.socials}>
+              {values.map((value, index) => {
+                const key = keys[index];
+
+                return (
+                  <button className={styles.social} key={key}>
+                    {value}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
