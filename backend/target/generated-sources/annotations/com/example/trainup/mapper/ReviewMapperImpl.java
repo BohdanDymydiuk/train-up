e4bef3abba42ad867f6a7ad92101e0aa6b43c6aa
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2025-08-30 16:58:56 +0300

    add changes

--- backend/target/generated-sources/annotations/com/example/trainup/mapper/ReviewMapperImpl.java
+++ backend/target/generated-sources/annotations/com/example/trainup/mapper/ReviewMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-08-28T12:20:45+0200",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
+    date = "2025-08-28T17:42:54+0000",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.16 (Ubuntu)"
 )
 @Component
 public class ReviewMapperImpl implements ReviewMapper {
